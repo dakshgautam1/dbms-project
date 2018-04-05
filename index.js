da2642550050b099144c6458b18e7d41b3cf6a35
dakshgautam1 <dakshcoder@gmail.com>
2018-04-05 16:16:02 -0400

    Test connection oracle established

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
 const oracledb = require('oracledb');
-
 const username = process.env.username || '<your_user_name>'
 const password = process.env.password || '<your_oracle_password>'
+const express = require('express')
+const app = express()
 
-oracledb.getConnection(
-  {
-    user          : username,
-    password      : password,
-    connectString : "oracle.cise.ufl.edu:1521/orcl"
-  },
-  function(err, connection)
-  {
-    if (err) {
-      console.error(err.message);
-      return;
+queryExecuteWithOracle = (query, params, applyFunction) => {
+
+  let pool = oracledb.getPool()
+  return pool.getConnection()
+  .then((conn) => conn.execute(query, params))
+  .then(result => result.rows)
+  .then(result => applyFunction(result))
+  .then(result => result)
+  .catch(err => {
+    return {
+      "error": err,
+      "type": "Some kind of error occured with query/connection pooling"
     }
-    connection.execute(
-      `SELECT * FROM city`,  // bind value for :id
-      function(err, result)
-      {
-        if (err) {
-          console.error(err.message);
-          doRelease(connection);
-          return;
-        }
-        console.log(result.rows);
-        doRelease(connection);
-      });
-  });
+  })
 
-function doRelease(connection)
-{
-  connection.close(
-    function(err) {
-      if (err)
-        console.error(err.message);
-    });
 }
+
+
+transform = (result) => {
+  return result;
+}
+
+app.get('/test', (req, res) => {
+  queryExecuteWithOracle('select * from city', [], transform)
+  .then((result) => {
+    res.send({
+      "result": result
+    })
+  })
+  .catch(err => {
+    res.send({
+      "error": "Some error with the connection setup"
+    })
+  })
+
+})
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  oracledb.createPool(
+    {
+      user          : username,
+      password      : password,
+      connectString : "oracle.cise.ufl.edu:1521/orcl"
+    }
+  ).then(() => {
+    console.log('established pool')
+    console.log('Starting server on ..... ' + PORT)
+  })
+})
+
+process.on('SIGINT', () => {
+  log.info('releasing Oracle pool')
+  poolP
+  .then(pool => BP.fromNode(cb => pool.terminate(cb)))
+  .then(() => process.exit(0), e => {throw e})
+})
