4b49d8291a874dead7cb6e4e4d8c00cb316eccb6
dakshgautam1 <dakshcoder@gmail.com>
2018-04-16 16:50:24 -0400

    DB connections in connection pooling increased

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const username = process.env.username || '<your_user_name>'
 const password = process.env.password || '<your_oracle_password>'
 const dbuser = process.env.dbuser || 'manika.'
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 
 queryExecuteWithOracle = (query, params, applyFunction) => {
 
@@ -27,7 +47,18 @@ queryExecuteWithOracle = (query, params, applyFunction) => {
 
 
 transform = (result) => {
-  return result;
+  new_result = result.map(re => {
+
+    return {
+      cityName: re[0],
+      state: re[1],
+      latitude: re[3],
+      longitude: re[4],
+      region: re[5]
+    }
+  })
+
+  return new_result;
 }
 
 app.get('/test', (req, res) => {
@@ -45,12 +76,12 @@ app.get('/test', (req, res) => {
 
 })
 
-
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   oracledb.createPool(
     {
+      poolMax: 64,
       user          : username,
       password      : password,
       connectString : "oracle.cise.ufl.edu:1521/orcl"
