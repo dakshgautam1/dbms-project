a3f9a363d6d00a1137709a703c5f676a0b2d55a4
dakshgautam1 <dakshcoder@gmail.com>
2018-04-25 08:51:54 -0400

    Adding functionality for city.js

--- dgFunctions.js
+++ dgFunctions.js
@@ -1,15 +1,12 @@
-query1 = `select * from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER`;
+const query1 = `select * from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER`;
 
-query2 = `select * from (select c1.w_date as w_date, c1.f1 as city1_values, c1.city1, c2.f2  as city2_values, c2.city2 from ((select w_date, metric_value as f1, city_name as city1 from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect1 and city_name= :cityName1) c1) FULL OUTER JOIN ((select w_date, metric_value as f2, city_name as city2 from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect2 and city_name= :cityName2) c2) ON c1.w_date=c2.w_date)`;
+//const query2 = `select * from (select c2.aspect, c1.w_date as w_date, c1.f1 as city1_values, c1.city1, c2.f2  as city2_values, c2.city2, c2.unit from ((select w_date, metric_value as f1, city_name as city1, unit, aspect from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect1 and city_name= :cityName1) c1) FULL OUTER JOIN ((select w_date, metric_value as f2, city_name as city2, unit, aspect from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect2 and city_name= :cityName2) c2) ON c1.w_date=c2.w_date)`;
 
+const query2 = `select aspect, w_date, m1, city_name1, m2, city_name2, unit from (select v1.aspect as aspect, v1.w_date as w_date, v1.m as m1, v1.city_name as city_name1, v2.m as m2, v2.city_name as city_name2, v1.unit as unit  from (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, m, city_name, aspect, unit from (select to_char(w_date, 'yyyy') as x, to_char(w_date, 'mm') as y, avg(metric_value) as m, city_name, aspect, unit from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect1 and city_name=:city1 group by to_char(w_date, 'yyyy'), to_char(w_date, 'mm'), city_name, aspect, unit order by to_char(w_date, 'yyyy'))) v1 FULL OUTER JOIN (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, m, city_name, aspect, unit from (select to_char(w_date, 'yyyy') as x, to_char(w_date, 'mm') as y, avg(metric_value) as m, city_name, aspect, unit from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect2 and city_name= :city2 group by to_char(w_date, 'yyyy'), to_char(w_date, 'mm'), city_name, aspect, unit order by to_char(w_date, 'yyyy'))) v2 ON v1.w_date = v2.w_date)`
 
-query3 = `select * from (select t1.w_date, t1.a1, t1.u1, t1.m1, t2.a2, t2.u2, t2.m2 from 
-  (((select w_date, aspect as a1, unit as u1, metric_value as m1 from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER
-  where aspect = 'Temperature' and city_name='New York') t1) 
-  FULL OUTER JOIN
-  ((select w_date, aspect as a2, unit as u2, metric_value as m2 from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER
-  where aspect = 'Humidity' and city_name='New York') t2) ON t1.w_date = t2.w_date))
-  ;`
+//const query3 = `select * from (select t2.city_name, t1.w_date, t1.a1, t1.u1, t1.m1, t2.a2, t2.u2, t2.m2 from (((select w_date, aspect as a1, unit as u1, metric_value as m1, city_name from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect1 and city_name=:c1) t1) FULL OUTER JOIN ((select w_date, aspect as a2, unit as u2, metric_value as m2, city_name from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect2 and city_name=:c2) t2) ON t1.w_date = t2.w_date))`
+
+const query3 = `select city_name, w_date, aspect1, unit1, m1, aspect2, unit2, m2 from (select v1.city_name as city_name, v1.w_date as w_date, v1.aspect as aspect1, v1.unit as unit1, v1.m as m1, v2.aspect as aspect2, v2.unit as unit2, v2.m as m2 from (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, m, city_name, aspect, unit from (select to_char(w_date, 'yyyy') as x, to_char(w_date, 'mm') as y, avg(metric_value) as m, city_name, aspect, unit from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect1 and city_name=:city1 group by to_char(w_date, 'yyyy'), to_char(w_date, 'mm'), city_name, aspect, unit order by to_char(w_date, 'yyyy'))) v1 FULL OUTER JOIN (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, m, city_name, aspect, unit from (select to_char(w_date, 'yyyy') as x, to_char(w_date, 'mm') as y, avg(metric_value) as m, city_name, aspect, unit from MANIKA.CITY NATURAL JOIN MANIKA.IS_AFFECTED_BY NATURAL JOIN MANIKA.WEATHER where aspect = :aspect2 and city_name= :city2 group by to_char(w_date, 'yyyy'), to_char(w_date, 'mm'), city_name, aspect, unit order by to_char(w_date, 'yyyy'))) v2 ON v1.w_date = v2.w_date)`;
 
 
 const firstUpperCase = input => input[0].toUpperCase() + input.substr(1);
@@ -22,28 +19,44 @@ const addOrderByClause = (query, key, isAscending) =>
 
 
 
-const handleDatesClause = (query, queryArguments, dates) => {
+const handleDatesClause = (query, queryArguments, dates, appendAnd=true) => {
 
   if (dates.length !== 0) {
     if (dates.length === 1) {
       let d = new Date(dates[0]);
       if (new Date().getFullYear() === d.getFullYear()) {
-        query += ` and to_char(W_DATE, 'mm') = :f1`;
+        if (appendAnd) {
+          query += ` and to_char(W_DATE, 'mm') = :f1`;
+        } else {
+          query += ` to_char(W_DATE, 'mm') = :f1`;
+        }
         queryArguments.push(d.getMonth());
       } else {
         // handling for year only. 
-        query += ` and to_char(W_DATE, 'yyyy') = :f1`;
+        if (appendAnd) {
+          query += ` and to_char(W_DATE, 'yyyy') = :f1`;
+        } else {
+          query += ` to_char(W_DATE, 'yyyy') = :f1`;
+        }
         queryArguments.push(d.getFullYear());
       }
     } else if (dates.length === 2) {
       let d1 = new Date(dates[0]),
           d2 = new Date(dates[1]);
 
-          if (new Date().getFullYear() <= d1.getFullYear()) {
-        query += ` and to_char(W_DATE, 'mm') >= :d1 and to_char(W_DATE, 'mm') <= :d2`;
+      if (new Date().getFullYear() <= d1.getFullYear()) {
+        if (appendAnd) {
+          query += ` and to_char(W_DATE, 'mm') >= :d1 and to_char(W_DATE, 'mm') <= :d2`;
+        } else {
+          query += ` to_char(W_DATE, 'mm') >= :d1 and to_char(W_DATE, 'mm') <= :d2`;
+        }
         queryArguments.push(d1.getMonth(), d2.getMonth());
       } else {
-        query += ` and to_char(W_DATE, 'yyyy') >= :d1 and to_char(W_DATE, 'yyyy') < :d2`;
+        if (appendAnd) {
+          query += ` and to_char(W_DATE, 'yyyy') >= :d1 and to_char(W_DATE, 'yyyy') < :d2`;
+        } else {
+          query += ` to_char(W_DATE, 'yyyy') >= :d1 and to_char(W_DATE, 'yyyy') < :d2`;
+        }
         queryArguments.push(d1.getFullYear(), d2.getFullYear());
       }
     }
@@ -96,11 +109,15 @@ generateTwoCityJoinWithSameAttributes = (
 
   queryArguments.push(firstUpperCase(weatherAspects[0]), cityNames[0], firstUpperCase(weatherAspects[0]), cityNames[1])
 
-  // afterAddingDateClauses = handleDatesClause(query, queryArguments, dates);
-  // query = afterAddingDateClauses.query
-  // queryArguments = afterAddingDateClauses.queryArguments
+  if (dates.length != 0) {
+    query += " where "
+    afterAddingDateClauses = handleDatesClause(query, queryArguments, dates, false);
+    query = afterAddingDateClauses.query
+    queryArguments = afterAddingDateClauses.queryArguments
+  }
 
-  // query = addOrderByClause(query, "w_date", true);
+
+  query = addOrderByClause(query, "w_date", true);
 
   return {
     query: query,
@@ -110,7 +127,38 @@ generateTwoCityJoinWithSameAttributes = (
   
 }
 
+generateSameCityTwoAttributes = (
+  cityNames,
+  regionNames,
+  weatherAspects,
+  dates,
+  tables
+) => {
+
+  let query = query3.slice(), 
+      queryArguments = [];
+
+      queryArguments.push(firstUpperCase(weatherAspects[0]), cityNames[0], firstUpperCase(weatherAspects[1]), cityNames[0])
+
+      if (dates.length != 0) {
+        query += " where "
+        afterAddingDateClauses = handleDatesClause(query, queryArguments, dates, false);
+        query = afterAddingDateClauses.query
+        queryArguments = afterAddingDateClauses.queryArguments
+      }
+    
+    
+      query = addOrderByClause(query, "w_date", true);
+
+  return {
+    query: query,
+    queryArguments: queryArguments,
+    graphtype: 3
+  }
+  
+}
 module.exports = {
   generateQuery: generateQuery,
-  generateTwoCityJoinWithSameAttributes: generateTwoCityJoinWithSameAttributes
+  generateTwoCityJoinWithSameAttributes: generateTwoCityJoinWithSameAttributes,
+  generateSameCityTwoAttributes: generateSameCityTwoAttributes
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const oracledb = require("oracledb");
 const express = require("express");
 const app = express();
+oracledb.autoCommit = true
 
 const username = process.env.username || "<your_user_name>";
 const password = process.env.password || "<your_oracle_password>";
@@ -9,13 +10,14 @@ var bodyParser = require("body-parser");
 
 const {
   generateQuery,
-  generateTwoCityJoinWithSameAttributes
+  generateTwoCityJoinWithSameAttributes,
+  generateSameCityTwoAttributes
 } = require("./dgFunctions");
 
 // Add headers
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
   // Request methods you wish to allow
   res.setHeader(
@@ -116,6 +118,24 @@ app.get("/test", (req, res) => {
     });
 });
 
+
+app.post("/test1", (req, res) => {
+
+  const query = `select * from ((select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, city_name, s1, pn1, mv1, mh1, aqi1 from (select to_char(po_date, 'yyyy') as x, to_char(po_date, 'mm') as y, city_name as city_name, symbol as s1, pollutant_name as pn1, avg(mean_value) as mv1, max(max_value) as mx1, avg(max_hour) as mh1, avg(aqi) as aqi1 from manika.city NATURAL JOIN MANIKA.IS_POLLUTED_BY NATURAL JOIN MANIKA.POLLUTANT where city_name=:city1 and symbol = 'NO2' and pollutant_name = 'Nitrogen Dioxide' group by to_char(po_date, 'yyyy'), to_char(po_date, 'mm'), city_name, symbol, pollutant_name order by to_char(po_date, 'yyyy'))) NATURAL JOIN (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, city_name, s2, pn2, mv2, mh2, aqi2 from (select to_char(po_date, 'yyyy') as x, to_char(po_date, 'mm') as y, city_name as city_name, symbol as s2, pollutant_name as pn2, avg(mean_value) as mv2, max(max_value) as mx2, avg(max_hour) as mh2, avg(aqi) as aqi2 from manika.city NATURAL JOIN MANIKA.IS_POLLUTED_BY NATURAL JOIN MANIKA.POLLUTANT where city_name=:city2 and symbol = 'SO2' and pollutant_name = 'Sulfur Dioxide' group by to_char(po_date, 'yyyy'), to_char(po_date, 'mm'), city_name, symbol, pollutant_name order by to_char(po_date, 'yyyy'))) NATURAL JOIN (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, city_name, s3, pn3, mv3, mh3, aqi3 from (select to_char(po_date, 'yyyy') as x, to_char(po_date, 'mm') as y, city_name as city_name, symbol as s3, pollutant_name as pn3, avg(mean_value) as mv3, max(max_value) as mx3, avg(max_hour) as mh3, avg(aqi) as aqi3 from manika.city NATURAL JOIN MANIKA.IS_POLLUTED_BY NATURAL JOIN MANIKA.POLLUTANT where city_name=:city3 and symbol = 'O3' and pollutant_name = 'Ozone' group by to_char(po_date, 'yyyy'), to_char(po_date, 'mm'), city_name, symbol, pollutant_name order by to_char(po_date, 'yyyy'))) NATURAL JOIN (select LAST_DAY(TO_DATE(x || y || '-01', 'YYYY-MM-DD')) as w_date, city_name, s4, pn4, mv4, mh4, aqi4 from (select to_char(po_date, 'yyyy') as x, to_char(po_date, 'mm') as y, city_name as city_name, symbol as s4, pollutant_name as pn4, avg(mean_value) as mv4, max(max_value) as mx4, avg(max_hour) as mh4, avg(aqi) as aqi4 from manika.city NATURAL JOIN MANIKA.IS_POLLUTED_BY NATURAL JOIN MANIKA.POLLUTANT where city_name=:city4 and symbol = 'CO' and pollutant_name = 'Carbon Monoxide' group by to_char(po_date, 'yyyy'), to_char(po_date, 'mm'), city_name, symbol, pollutant_name order by to_char(po_date, 'yyyy')))) WHERE w_date BETWEEN TO_DATE (:d1, 'mm/dd/yyyy') AND TO_DATE (:d2, 'mm/dd/yyyy')`
+  queryExecuteWithOracle(query, ['New York', 'New York','New York','New York', '02/28/2014', '02/28/2014'], transform)
+    .then(result => {
+      console.log(result)
+      res.send({
+        result: result
+      });
+    })
+    .catch(err => {
+      res.send({
+        error: "Some error with the connection setup"
+      });
+    });
+});
+
 transformToCityJson = result => {
   console.log(result);
   return result;
@@ -136,48 +156,12 @@ app.get("/get-cities", (req, res) => {
 });
 
 app.post("/get-city-description", (req, res) => {
-  console.log("Ye bhais aab", req.body);
   res.send({
     ye: "helo"
   });
 });
 
 passResults = results => results;
-
-transformSearchQueryResults = results => {
-  if (results.length === 0) {
-    return {
-      error: "No data found !"
-    };
-  }
-
-  let title, yAxisName, xAxisName, unit, seriesTitle;
-
-  let data = [];
-
-  title = results[0][0];
-  unit = results[0][9];
-
-  yAxisName = `${title} ( ${unit} )`;
-  seriesTitle = `${title} for the range specified Range`;
-
-  data = results.map(value => {
-    var d = new Date(value[1]);
-    return [
-      Date.UTC(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()),
-      value[8]
-    ];
-  });
-  console.log(data);
-  return {
-    title: title,
-    data: data,
-    xAxisName: "Year",
-    yAxisName: yAxisName,
-    unit: unit
-  };
-};
-
 
 app.post("/get-search-results", (req, res) => {
   console.log("Ye bhais aab", req.body.entities);
@@ -237,6 +221,31 @@ app.post("/get-search-results", (req, res) => {
         });
 
     } else if (weatherAspects.length === 2) {
+
+      let { query, queryArguments, graphtype } = generateSameCityTwoAttributes(
+        cityNames,
+        regionNames,
+        weatherAspects,
+        dates,
+        tables
+      );
+      console.log(queryArguments, query);
+
+      queryExecuteWithOracle(query, queryArguments, passResults)
+        .then(result => {
+          console.log(result.slice(1,5));
+          let makeResponse = transformTwoAttributes(result);
+          res.send({
+            result: makeResponse,
+            graphtype: graphtype
+          });
+        })
+        .catch(err => {
+          res.send({
+            error: "Some error with the connection setup."
+          });
+        });
+
     } else {
       let { query, queryArguments, graphtype } = generateQuery(
         cityNames,
@@ -249,6 +258,7 @@ app.post("/get-search-results", (req, res) => {
 
       queryExecuteWithOracle(query, queryArguments, passResults)
         .then(result => {
+          
           let makeResponse = transformSearchQueryResults(result);
           res.send({
             result: makeResponse,
@@ -265,10 +275,232 @@ app.post("/get-search-results", (req, res) => {
   }
 });
 
-transformTwoCityResult = (results) => {
+handleTableFormat = (table, result) => {
+  
+  if (table === 'City' || table === 'city') {
+    let headers = ['City', 'State', 'County', 'Latitude', 'Longitude', 'Region']
+    let sequence = ['city_name', 'state', 'county', 'latitude', 'longitude', 'region_name']
+    let primaryFields = ['city_name', 'state']
 
-  return results;
+    let finalResults = result.map(city => {
+      return {
+        'city_name': city[0],
+        'state': city[1],
+        'county': city[2],
+        'latitude': city[3],
+        'longitude': city[4],
+        'region_name': city[5]
+      }
+    })
+    //console.log(finalResults)
+    return {
+      "data": finalResults,
+      "headers": headers,
+      "primary": primaryFields,
+      "sequence": sequence
+    }
+
+  } else if (table === 'Population' || table === 'population') {
+    console.log('got here')
+    let headers = ['City', 'State', 'TimeStamp', 'Size']
+    let sequence = ['city_name', 'state', 'p_year', 'p_size']
+    let primaryFields = ['city_name', 'state', 'p_year']
+
+    let finalResults = result.map(city => {
+      console.log(city[2], new Date(city[2]))
+      let d = new Date(city[2])
+      return {
+        'city_name': city[0],
+        'state': city[1],
+        'p_year': d.getFullYear() + "-" + d.getUTCMonth() + "-" + d.getUTCDate(),
+        'p_size': city[3],
+        'orignal': city[2]
+      }
+    })
+
+    finalResults = finalResults.sort(function(a, b) {
+      return a['orignal'] - b['orignal']
+    })
+
+    return {
+      "data": finalResults,
+      "headers": headers,
+      "primary": primaryFields,
+      "sequence": sequence
+    }
+  } else if (table === 'is_affected_by' || table === 'Is_affected_by') {
+
+    result = result.slice(0, 500);
+    let headers = ['Metric Value', 'City Name', 'State', 'Aspect', 'TimeStamp']
+    let sequence = ['metric_value', 'city_name', 'state', 'aspect', 'w_date']
+    let primaryFields = ['city_name', 'state']
+
+    let finalResults = result.map(city => {
+      console.log(city[2], new Date(city[2]))
+      let d = new Date(city[4])
+      return {
+        'metric_value': city[0],
+        'city_name': city[1],
+        'state': city[2],
+        'aspect': city[3],
+        'w_date': d.getFullYear() + "-" + d.getUTCMonth() + "-" + d.getUTCDate(),
+        'orignal': d
+      }
+    })
+
+    finalResults = finalResults.sort(function(a, b) {
+      return a['orignal'] - b['orignal']
+    })
+
+    return {
+      "data": finalResults,
+      "headers": headers,
+      "primary": primaryFields,
+      "sequence": sequence
+    }
+  } else if (table === 'is_polluted_by') {
+    result = result.slice(0, 500);
+    let headers = ['Symbol', 'Date', 'City Name', 'State', 'Mean Value', 'Max Value', 'Max Hour', 'AQI']
+    let sequence = ['symbol', 'po_date', 'city_name', 'state', 'mean_value', 'max_value', 'max_hour', 'aqi']
+    let primaryFields = ['city_name', 'state']
+
+    let finalResults = result.map(city => {
+      console.log(city[2], new Date(city[2]))
+      let d = new Date(city[1])
+      return {
+        'symbol': city[0],
+        'po_date': d.getFullYear() + "-" + d.getUTCMonth() + "-" + d.getUTCDate(),
+        'city_name': city[2],
+        'state': city[3],
+        'mean_value': city[4],
+        'max_value': city[5],
+        'max_hour': city[6],
+        'aqi': city[7],
+        'orignal': d
+      }
+    })
+
+    finalResults = finalResults.sort(function(a, b) {
+      return a['orignal'] - b['orignal']
+    })
+
+    return {
+      "data": finalResults,
+      "headers": headers,
+      "primary": primaryFields,
+      "sequence": sequence
+    }
+  }
+  return result;
 }
+
+
+app.post('/get-table', (req, res) => {
+  if ('table' in req.body) {
+    const table = req.body.table
+    queryExecuteWithOracle(`select * from ${dbuser}${table}`, [], passResults)
+    .then(result => {
+      const finalResults = handleTableFormat(table, result);
+      res.send({
+        result: finalResults
+      });
+    })
+    .catch(err => {
+      res.send({
+        error: "Some error with the connection setup."
+      });
+    });
+  } else {
+    res.send({
+      error: "Some problem with api connection"
+    })
+  }
+
+})
+
+app.post('/add-city', (req, res) => {
+  console.log(req.body, "Hi i am here")
+  if ('params' in req.body) {
+    const params = req.body.params;
+    queryExecuteWithOracle(`insert into ${dbuser}city values (:city_name, :state, :county, :latitude, :longitude, :region_name)`, params, passResults)
+    .then(result => {
+      console.log(result, ' I am the query result')
+      res.send({
+        result: result
+      });
+    })
+    .catch(err => {
+      res.send({
+        error: "Some error with the connection setup."
+      });
+    });
+  } else {
+    res.send({
+      "ylo": "Hi man"
+    })
+  }
+})
+
+
+app.post('/update-is-affected', (req, res) => {
+
+  if ('city_name' in req.body && 'orignal' in req.body && 'aspect' in req.body && 'metric_value' in req.body) {
+    let city_name = req.body.city_name;
+    let orignal = req.body.orignal;
+    let aspect = req.body.aspect;
+    let new_value = req.body.metric_value
+    let d = new Date(orignal)
+    d.setMonth(d.getMonth() + 1);
+    let  = `${d.getMonth()}-${d.da()}-${d.getFullYear()}`;
+    condensed_date = condensed_date.trim()
+    console.log(condensed_date, new_value, "these are teh value")
+    
+    queryExecuteWithOracle(`update ${dbuser}is_affected_by set metric_value = :id1 where city_name = :id2 and aspect = :id3 and w_date = TO_DATE(:id4,'dd/mm/yy')`, 
+    [Number(new_value), city_name, aspect, condensed_date], passResults)
+    .then(results => {
+
+      res.send({
+        result: result
+      });    
+    }).catch(err => {
+      res.send({
+        error: "Some error with the connection setup."
+      });
+    });
+
+  }
+  
+})
+
+
+app.post('/add-row', (req, res) => {
+  if ('table' in req.body && 'paramDict' in req.body && 'paramKeys' in req.body) {
+    const table = req.body.table;
+    const paramDict = req.body.paramDict;
+    const paramKeys = req.body.paramKeys;
+    let paramKeysQ = paramKeys.map(p => ":" + p);
+    paramKeysQ = paramKeysQ.join(", ");
+    console.log(paramDict, 'This is hte param dict')
+
+    queryExecuteWithOracle(`insert into ${dbuser}${table} values (${paramKeysQ}) `, paramDict, passResults)
+    .then(result => {
+      console.log(result, ' I am the query result')
+      res.send({
+        result: result
+      });
+    })
+    .catch(err => {
+      res.send({
+        error: "Some error with the connection setup."
+      });
+    });
+  } else {
+    res.send({
+      "ylo": "Hi man"
+    })
+  }
+});
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -295,3 +527,151 @@ process.on("SIGINT", () => {
     }
   );
 });
+
+
+/*
+  Transformations for different queries. 
+*/
+
+transformTwoAttributes = results => {
+  if (results.length === 0) {
+    return {
+      error: "No data found !"
+    };
+  }
+
+
+  let title, yAxisName, xAxisName, att1, att1Unit, att2, att2Unit, cityName;
+
+  // let data = [];
+  console.log("yaha aa gya mein")
+
+  att1 = results[0][2];
+  att1Unit = results[0][3];
+  att2 = results[0][5];
+  att2Unit = results[0][6];
+  cityName = results[0][0];
+
+  yAxisName = `${att1} (${att1Unit}) and ${att2} (${att2Unit})`
+  title = `${cityName} : ${att1} V/S ${att2}`
+  console.log(results, 'ither')
+
+  let attData1 = results.map(value => {
+    var d = new Date(value[1]);
+    return [
+      Date.UTC(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()),
+      value[4]
+    ];
+  })
+
+  console.log(attData1)
+  let attData2 = results.map(value => {
+    var d = new Date(value[1]);
+    return [
+      Date.UTC(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()),
+      value[7]
+    ];
+  })
+
+console.log(title)
+  let re =  {
+    title: title,
+    name1: `${att1} (${att1Unit})`,
+    data1: attData1,
+    name2: `${att2} (${att2Unit})`,
+    data2: attData2,
+    xAxisName: "Year",
+    yAxisName: yAxisName,
+
+  };
+
+  console.log(re)
+  return re;
+}
+
+transformSearchQueryResults = results => {
+  if (results.length === 0) {
+    return {
+      error: "No data found !"
+    };
+  }
+
+  let title, yAxisName, xAxisName, unit, seriesTitle;
+
+  let data = [];
+
+  title = results[0][0];
+  unit = results[0][9];
+
+  yAxisName = `${title} ( ${unit} )`;
+  seriesTitle = `${title} for the range specified Range`;
+
+  data = results.map(value => {
+    var d = new Date(value[1]);
+    return [
+      Date.UTC(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()),
+      value[8]
+    ];
+  });
+  console.log(data);
+  return {
+    title: title,
+    data: data,
+    xAxisName: "Year",
+    yAxisName: yAxisName,
+    unit: unit
+  };
+};
+
+transformTwoCityResult = (results) => {
+  if (results.length === 0) {
+    return {
+      error: "No data found !"
+    };
+  }
+  console.log(results)
+  let title, 
+      yAxisName, 
+      xAxisName, 
+      unit, 
+      seriesTitle,
+      cityName1,
+      cityName2;
+
+  // let data = [];
+
+  unit = results[0][6];
+  cityName1 = results[0][3];
+  cityName2 = results[0][5];
+  title = `${results[0][0]} Comparison between ${cityName1} and ${cityName2}`;
+  yAxisName = `${results[0][0]} ( ${unit} )`;
+  seriesTitle = `${title} for the range specified Range`;
+
+  let city1Data = results.map(value => {
+    let d = new Date(value[1]);
+       return [
+      Date.UTC(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()),
+      value[2]-5
+    ];
+  })
+  let city2Data = results.map(value => {
+    let d = new Date(value[1]);
+       return [
+      Date.UTC(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()),
+      value[4]
+    ];
+  })
+  
+  
+  return {
+    title: title,
+    name1: cityName1,
+    data1: city1Data,
+    name2: cityName2,
+    data2: city2Data,
+    xAxisName: "Year",
+    yAxisName: yAxisName,
+    unit: unit,
+  };
+}
+
